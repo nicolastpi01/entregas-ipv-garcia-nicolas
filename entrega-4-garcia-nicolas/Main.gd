f920735a12d7448f85401df8b837311701b1a538
nicolas <nicolastpi10@gmail.com>
2023-04-13 03:30:47 -0300

    torreta dispara

--- entrega-4-garcia-nicolas/Main.gd
+++ entrega-4-garcia-nicolas/Main.gd
@@ -9,6 +9,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Player.set_projectile_container(self)
+	$Turret.set_values($Player, self)
 	#pass # Replace with function body.
 
 
