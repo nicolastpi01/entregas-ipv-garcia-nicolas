dd5088e1930eedfb84d7fa45763893f9b9bf52a3
nicolas <nicolastpi10@gmail.com>
2023-04-13 03:43:21 -0300

    borro comentarios innecesarios

--- entrega-4-garcia-nicolas/entities/player/AbstractProjectile.gd
+++ entrega-4-garcia-nicolas/entities/player/AbstractProjectile.gd
@@ -7,17 +7,9 @@ signal delete_requested(projectile)
 export (float) var speed
 
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var direction: Vector2
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	#pass
 	set_physics_process(false)
 
 func set_starting_values(starting_position:Vector2, direction:Vector2):
@@ -27,13 +19,8 @@ func set_starting_values(starting_position:Vector2, direction:Vector2):
 	set_physics_process(true)
 	
 func _physics_process(delta):
-	#position.x += 50 * delta
 	position += direction*speed*delta
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_Timer_timeout():
 	emit_signal("delete_requested", self)
-	#pass # Replace with function body.
--- entrega-4-garcia-nicolas/entities/player/Cannon.gd
+++ entrega-4-garcia-nicolas/entities/player/Cannon.gd
@@ -1,9 +1,7 @@
 extends Sprite
 
 onready var fire_position: Position2D = $FirePosition
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 export (PackedScene) var projectile_escene: PackedScene
 #var projectile_scene: PackedScene = preload("res://entities/player/Projectile.tscn")
 
@@ -12,9 +10,7 @@ var projectile_container:Node
 
 func fire():
 	var projectile_instance:Projectile = projectile_escene.instance()
-	#fire_position.add_child(projectile_instance)
-	#print(projectile_instance)
-	#fire_position.add_child(projectile_instance)
+	
 	projectile_container.add_child(projectile_instance)
 	projectile_instance.set_starting_values(fire_position.global_position, (fire_position.global_position - global_position).normalized())
 	projectile_instance.connect("delete_requested", self, "_on_projectile_delete_requested")
@@ -24,6 +20,3 @@ func _on_projectile_delete_requested(projectile):
 	projectile.queue_free()
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
